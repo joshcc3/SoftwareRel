fde63e710a64d01cab39475bf0a2d1f8bbabeef1
Joshua Coutinho <jrc12@imperial.ac.uk>
2015-10-18 17:31:15 +0100

    Added scoping information to the type checker

--- HSRTool/TypeChecker/TCheckStmt.hs
+++ HSRTool/TypeChecker/TCheckStmt.hs
@@ -8,18 +8,25 @@ import Control.Monad.Writer
 import HSRTool.TypeChecker.TCheckExpr
 
 checkTypeStmt :: Stmt String -> TypeChecker SCType
-checkTypeStmt (SVarDecl v) = do
+checkTypeStmt (SVarDecl (VarDecl v)) = do
   s <- get
-  put (s{varMap = insert v SCInt s})
-  return SCUnit
+  let tInfo = typeInfo s
+  maybe 
+   (do
+     put (s{typeInfo = tInfo { varMap = insert v SCInt (scope s) s}})
+     return SCUnit)
+   (\_ -> tell ["Redeclaraion of variable within scope: " ++ v] >> return SCAny)
+   (lkup tInfo v (scope s))
 checkTypeStmt (SAssignStmt (AssignStmt id e)) = do
-  s <- get
+  st <- get
+  let s = typeInfo st
+      sc = scope st
   maybe 
    (tell ["Undeclared variable: " ++ id] >> return SCAny)
    (\t -> do
      r <- checkTypes (e, t)
      maybe (return SCAny) return r)
-   (varMap s id)
+   (lkup (varMap s) id sc)
 checkTypeStmt (SAssertStmt (AssertStmt e)) = do
   mt <- checkTypes (e, SCBool)
   maybe 
@@ -30,21 +37,40 @@ checkTypeStmt (SAssumeStmt (AssumeStmt e)) = do
   t <- checkTypeExpr e
   return t
 checkTypeStmt (SHavocStmt (HavocStmt id)) = do
-  s <- get
+  s <- (typeInfo <$> get)
   maybe 
    (tell ["Undeclared variable: " ++ id] >> return SCAny)
    return
    (varMap s id)
 checkTypeStmt (SIfStmt (IfStmt b t e)) = do
-  s <- get
+  st <- get
   b' <- checkTypes (b, SCBool)
-  maybe 
-   (tell ["If condition should be a bool"])
-   (\_ -> return ())
-   b'
+  newScope
+  let s = typeInfo st
+  maybe (tell ["If condition should be a bool"]) (\_ -> return ()) b'
   ts <- mapM checkTypeStmt t
-  ets <- maybe (return []) id (mapM checkTypeStmt <$> e) 
+  closeScope
+  newScope
+  ets <- maybe (return []) id (mapM checkTypeStmt <$> e)
+  closeScope
   return SCUnit
 checkTypeStmt (SBlockStmt s) = do
+  newScope
   x <- mapM checkTypeStmt s
+  closeScope
   return (last x)
+
+newScope = do
+  st <- get
+  let stack = scopeStack st
+  if null stack
+  then put $ st { scope = 0, scopeStack = [] }
+  else put $ st { scope = scope st + 1, scopeStack = scope st:stack }
+
+closeScope = do
+  st <- get
+  let stack = scopeStack st
+  if null stack
+  then error "Attempted to close a scope without any open scope"
+  else put $ st { scope = head (scopeStack st), 
+                  scopeStack = tail (scopeStack st) }
