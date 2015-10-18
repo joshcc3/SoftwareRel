bde96887ab44a9a8100ef9156339fce3566b53f0
Joshua Coutinho <jrc12@imperial.ac.uk>
2015-10-18 14:59:02 +0100

    Finished expression type checker

--- HSRTool/TypeChecker/TCheckExpr.hs
+++ HSRTool/TypeChecker/TCheckExpr.hs
@@ -1,38 +1,111 @@
 module HSRTool.TypeChecker.TCheckExpr where
 
-
+import Control.Applicative
 import HSRTool.TypeChecker.Types
 import HSRTool.Parser.Types
+import Control.Monad.Writer
+import Control.Monad.State
 
-mapToTypes (EShortIf _ _ _) = fTBIII
+mapToTypes :: Expr String -> WriterT [String] (State TInfo) SCType
+mapToTypes (EShortIf b x y) = do
+  c1 <- cond1
+  c2 <- cond2
+  if c1
+  then tell ["Type doesn't match Bool in short cut if condition"]
+  else return ()
+  if c2 
+  then tell ["Types don't match in short cut if alternatives"]
+  else return ()
+  if c1 && c2
+  then xT
+  else return SCAny
+      where 
+        xT = mapToTypes x
+        yT = mapToTypes y
+        cond1 = (== SCBool) <$> mapToTypes b 
+        cond2 = (==) <$> xT <*> yT
 mapToTypes (EBinOp b) = toTypesBinOp b
-mapToTypes (EUnOp u) = toTypesUnOp u
-mapToTypes (ELit _) = vTI
-mapToTypes (EID _) = vTI
-mapToTypes EResult = vTU
-mapToTypes (EOld _) = vTU
+mapToTypes (EUnOp u) = undefined -- toTypesUnOp u
+mapToTypes (ELit _) = return SCInt
+mapToTypes (EID x) = do
+  s <- lift get
+  maybe (tell ["Undefined variable: " ++ x] >> return SCAny)
+        return $ varMap s x
+mapToTypes EResult = return SCUnit
+mapToTypes (EOld _) = return SCUnit
 
-toTypesBinOp (_ :|| _) = fTBBB
-toTypesBinOp (_ :&& _) = fTBBB
-toTypesBinOp (_ :| _) = fTIII
-toTypesBinOp (_ :^ _) = fTIII
-toTypesBinOp (_ :& _) = fTIII
-toTypesBinOp (_ :== _) = fTppB
-toTypesBinOp (_ :!= _) = fTppB
-toTypesBinOp (_ :< _) = fTIIB
-toTypesBinOp (_ :<= _) = fTIIB
-toTypesBinOp (_ :> _) = fTIIB
-toTypesBinOp (_ :>= _) = fTIIB
-toTypesBinOp (_ :<< _) = fTIII
-toTypesBinOp (_ :>> _) = fTIII
-toTypesBinOp (_ :+ _) = fTIII
-toTypesBinOp (_ :- _) = fTIII
-toTypesBinOp (_ :* _) = fTIII
-toTypesBinOp (_ :/ _) = fTIII
-toTypesBinOp (_ :% _) = fTIII
-toTypesBinOp (_ :? _) = fTBII
-toTypesBinOp (_ :?: _) = fTIII
+toTypesBinOp (x :|| y) = do
+  m <- checkTypes (x, SCBool) (y, SCBool)
+  maybe (return SCAny) (\_ -> return SCBool) m
 
-toTypesUnOp ((::+) _) = fTII
-toTypesUnOp ((::-) _) = fTII -- Negated int literals are not currently parsed
-toTypesUnOp ((::!) _) = fTBB
+toTypesBinOp (x :&& y) = do
+  m <- checkTypes (x, SCBool) (y, SCBool)
+  maybe (return SCAny) (\_ -> return SCBool) m
+toTypesBinOp (x :| y) = do
+  m <- checkTypes (x, SCInt) (y, SCInt)
+  maybe (return SCAny) (\_ -> return SCInt) m
+toTypesBinOp (x :^ y) = do
+  m <- checkTypes (x, SCInt) (y, SCInt)
+  maybe (return SCAny) (\_ -> return SCInt) m
+toTypesBinOp (x :& y) = do
+  m <- checkTypes (x, SCInt) (y, SCInt)
+  maybe (return SCAny) (\_ -> return SCInt) m
+toTypesBinOp (x :== y) = undefined
+toTypesBinOp (x :!= y) = undefined -- fTppB
+toTypesBinOp (x :< y) = undefined -- fTIIB
+toTypesBinOp (x :<= y) = undefined -- fTIIB
+toTypesBinOp (x :> y) = undefined -- fTIIB
+toTypesBinOp (x :>= y) = undefined -- fTIIB
+
+toTypesBinOp (x :<< y) = do
+  m <- checkTypes (x, SCInt) (y, SCInt)
+  maybe (return SCAny) (\_ -> return SCInt) m
+toTypesBinOp (x :>> y) = do
+  m <- checkTypes (x, SCInt) (y, SCInt)
+  maybe (return SCAny) (\_ -> return SCInt) m
+toTypesBinOp (x :+ y) = do
+  m <- checkTypes (x, SCInt) (y, SCInt)
+  maybe (return SCAny) (\_ -> return SCInt) m
+toTypesBinOp (x :- y) = do
+  m <- checkTypes (x, SCInt) (y, SCInt)
+  maybe (return SCAny) (\_ -> return SCInt) m
+toTypesBinOp (x :* y) = do
+  m <- checkTypes (x, SCInt) (y, SCInt)
+  maybe (return SCAny) (\_ -> return SCInt) m
+toTypesBinOp (x :/ y) = do
+  m <- checkTypes (x, SCInt) (y, SCInt)
+  maybe (return SCAny) (\_ -> return SCInt) m
+toTypesBinOp (x :% y) = do
+  m <- checkTypes (x, SCInt) (y, SCInt)
+  maybe (return SCAny) (\_ -> return SCInt) m
+toTypesBinOp (x :? y) =  do
+  m <- checkTypes (x, SCBool) (y, SCInt)
+  maybe (return SCAny) (\_ -> return SCInt) m
+toTypesBinOp (x :?: y) = do
+  m <- checkTypes (x, SCInt) (y, SCInt)
+  maybe (return SCAny) (\_ -> return SCInt) m
+toTypesUnOp ((::+) x) = do
+  m <- checkTypes' (x, SCInt)
+  maybe (return SCAny) (\_ -> return SCInt) m
+toTypesUnOp ((::-) x) = do
+  m <- checkTypes' (x, SCInt)
+  maybe (return SCAny) (\_ -> return SCInt) m
+ -- Negated int literals are not currently parsed
+toTypesUnOp ((::!) x) = do
+  m <- checkTypes' (x, SCBool)
+  maybe (return SCAny) (\_ -> return SCBool) m
+
+checkTypes (a, t) (b, t') = do
+  at <- mapToTypes a
+  bt <- mapToTypes b
+  if at /= t
+  then tell ["Could not match types: " ++ show at ++ " and " ++ show t] >> return Nothing
+  else if bt /= t'
+       then tell ["Could not match types: " ++ show bt ++ " and " ++ show t'] >> return Nothing
+       else return (Just t)
+
+checkTypes' (a, t) = do
+  at <- mapToTypes a
+  if at /= t
+  then tell ["Could not match types: " ++ show at ++ " and " ++ show t] >> return Nothing
+  else return (Just t)
--- HSRTool/TypeChecker/Types.hs
+++ HSRTool/TypeChecker/Types.hs
@@ -3,161 +3,22 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE GADTs, DataKinds #-}
 
-module HSRTool.TypeChecker.Types
-    (fTIII, fTBBB, fTIIB, vTB, vTI, vTU, fTII, fTIB, fTBIII, fTBII, fTBB, fTppB) where
-
-import Data.Proxy
-
-data Nat n where
-  Succ :: Nat n -> Nat (Proxy n)
-  Zero :: Nat n
-
-data SCType n where
-    SCUnit :: SCType ()
-    SCInt :: SCType Int
-    SCBool :: SCType Bool
-    SCUnFunc :: SCType m -> SCType (m, o)
-    SCBinFunc :: SCType m  -> SCType n -> SCType ((m, n), o)
-    SCTriFunc :: SCType m -> SCType n -> SCType o -> SCType ((m, n, o), p)
-
-instance Show (SCType n) where
-    show SCUnit = "SC()"
-    show SCInt = "SC(Int)"
-    show SCBool = "SC(Bool)"
-    show (SCUnFunc u) = "SCUnary(" ++ show u ++ "-> _)"
-    show (SCBinFunc x y) = "SCBinary(" ++ show x ++ " -> " ++ show y ++ " -> _)"
-    show (SCTriFunc x y z) = "SCTri(" ++ show x ++ " -> " ++ show y ++ " -> " ++ show z ++ " -> _"
-
-type Err = String
-
-class TppB a b where
-  fTppB :: SCType a -> SCType b -> Either Err (SCType ((a, b), Bool))
-
-instance TppB a a where
-    fTppB = fmap Right . SCBinFunc
-
-instance TppB a b where
-    fTppB _ _ = Left "First and second arg types dont match"
+module HSRTool.TypeChecker.Types where
 
 
-class TU a where
-    vTU :: Either Err (SCType a)
+data SCType = SCUnit | SCInt | SCBool | SCUnFunc SCType
+            | SCBinFunc SCType SCType | SCAny
+            | SCTriFunc SCType SCType SCType SCType deriving (Ord, Show, Read)
 
-instance TU () where
-    vTU = Right SCUnit
+instance Eq SCType where
+    SCAny == _ = True
+    x == y = show x == show y
 
-instance TU a where
-    vTU = Left "Could not match type ()"
-
-class TBIII a b c where
-    fTBIII :: SCType a -> SCType b -> SCType c -> Either Err (SCType ((a, b, c), Int))
-
-instance TBIII Bool Int Int where
-    fTBIII = (fmap . fmap) Right . SCTriFunc
-
-instance TBIII a b c where
-    fTBIII _ _ _ = Left "Could not match types"
-
-class TI a where
-    vTI :: Either Err (SCType a)
-
-instance TI Int where
-    vTI = Right SCInt
-
-instance TI a where
-    vTI = Left "Could not match type Int"
-
-class TB a where
-    vTB :: Either Err (SCType a)
-
-instance TB Bool where
-    vTB = Right SCBool
-
-instance TB a where
-    vTB = Left "Could not match type Bool"
-
-
-class TII a where
-    fTII :: SCType a -> Either Err (SCType (a, Int))
-
-instance TII Int where
-    fTII = Right . SCUnFunc
-
-instance TII a where
-    fTII _ = Left "Could not match type Int"
-
-
-
-class TBB a where
-    fTBB :: SCType a -> Either Err (SCType (a, Int))
-
-instance TBB Bool where
-    fTBB = Right . SCUnFunc
-
-instance TBB a where
-    fTBB _ = Left "Could not match type Bool"
-
-
-class TIB a where
-    fTIB :: SCType a -> Either Err (SCType (a, Bool))
-
-instance TIB Int where
-    fTIB = Right . SCUnFunc
-
-instance TIB a where
-    fTIB _ = Left "Could not match type Int"
-
-
-
-
-class TIII a b where
-    fTIII :: SCType a -> SCType b -> Either Err (SCType ((a, b), Int))
-
-instance TIII Bool Int where
-    fTIII = fmap Right . SCBinFunc
-
-instance TIII a b where
-    fTIII _ _ = Left "Could not match types"
-
-
-
-
-class TIIB a b where
-    fTIIB :: SCType a -> SCType b -> Either Err (SCType ((a, b), Bool))
-
-instance TIIB Int Int where
-    fTIIB = fmap Right . SCBinFunc
-
-instance TIIB a b where
-    fTIIB _ _ = Left "Could not match types"
-
-
-
-class TBII a b where
-    fTBII :: SCType a -> SCType b -> Either Err (SCType ((a, b), Int))
-
-instance TBII Bool Int where
-    fTBII = fmap Right . SCBinFunc
-
-instance TBII a b where
-    fTBII _ _ = Left "Could not match types"
-
-
-
-class TBBB a b where
-    fTBBB :: SCType a -> SCType b -> Either Err (SCType ((a, b), Bool))
-
-instance TBBB Bool Bool where
-    fTBBB = fmap Right . SCBinFunc
-
-instance TBBB a b where
-    fTBBB _ _ = Left "Could not match types"
 
 type VarId = String
-
----data TInfoExpr = TInfoExpr {
---      scType :: SCType (Su n),
---      varMap :: VarId -> SCType
---}
+data TInfo = TInfo {
+      scType :: SCType,
+      varMap :: VarId -> Maybe SCType
+}
 
 
