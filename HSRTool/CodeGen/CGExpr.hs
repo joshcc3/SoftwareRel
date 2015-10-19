40cd9cca7f02877e41539392d9cf49bafa4d20e6
Pavan <pp2112@ic.ac.uk>
2015-10-19 23:11:28 +0100

    Almost there

--- HSRTool/CodeGen/CGExpr.hs
+++ HSRTool/CodeGen/CGExpr.hs
@@ -4,38 +4,35 @@ import HSRTool.Parser.Types
 
 evalBinOp :: (Show a) => (BinOp a) -> String
 evalBinOp (expr1 :|| expr2) = "(|| " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
-evalBinOp (expr1 :&& expr2) = "(&& " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
+evalBinOp (expr1 :&& expr2) = "(and " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
 evalBinOp (expr1 :| expr2) = "(| " ++ evalExpr expr1 ++ " " ++  evalExpr expr2 ++ ")"
 evalBinOp (expr1 :^ expr2) = "(^ " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
 evalBinOp (expr1 :& expr2) = "(& " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
 evalBinOp (expr1 :== expr2) = "(== " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
 evalBinOp (expr1 :!= expr2) = "(!= " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
-evalBinOp (expr1 :< expr2) = "(< " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
+evalBinOp (expr1 :< expr2) = "(bvsgt " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
 evalBinOp (expr1 :<= expr2) = "(<= " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
 evalBinOp (expr1 :> expr2) = "(> " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
 evalBinOp (expr1 :>= expr2) = "(>= " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
-evalBinOp (expr1 :<< expr2) = "(<< " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
-evalBinOp (expr1 :>> expr2) = "(>> " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
-evalBinOp (expr1 :+ expr2) = "(+ " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
-evalBinOp (expr1 :- expr2) = "(- " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
-evalBinOp (expr1 :* expr2) = "(* " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
+evalBinOp (expr1 :<< expr2) = "(bvshl " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
+evalBinOp (expr1 :>> expr2) = "(bvashr " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
+evalBinOp (expr1 :+ expr2) = "(bvadd " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
+evalBinOp (expr1 :- expr2) = "(bvsub " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
+evalBinOp (expr1 :* expr2) = "(bvmul " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
 evalBinOp (expr1 :/ expr2) = "(/ " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
-evalBinOp (expr1 :% expr2) = "(% " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
+evalBinOp (expr1 :% expr2) = "(bvsmod " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
 evalBinOp (expr1 :? expr2) = "(? " ++ evalExpr expr1 ++ " " ++ evalExpr expr2 ++ ")"
 
 evalUnOP :: (Show id) => (UnOp id) -> String
-evalUnOP ((::-) expr1) = "(- " ++ evalExpr expr1
+evalUnOP ((::-) expr1) = "(bvneg " ++ evalExpr expr1
 evalUnOP ((::+) expr1) = "(+ " ++ evalExpr expr1
-evalUnOP ((::!) expr1) = "(! " ++ evalExpr expr1
-evalUnOp ((::~) expr1) = "(~ " ++ evalExpr expr1
+evalUnOP ((::!) expr1) = "(not " ++ evalExpr expr1
+evalUnOp ((::~) expr1) = "(bvnot " ++ evalExpr expr1
 
 evalExpr :: (Show a) => (Expr a) -> String
 evalExpr (EBinOp binOP) = evalBinOp binOP
 evalExpr (EUnOp unOP) = evalUnOP unOP
-evalExpr (ELit x) = show x
+evalExpr (ELit x) = "_ bv" ++  show x ++ ")"
 evalExpr (EID id) = show id
-
-
-
 
 
