518a5b25303c8f35b2d7247a9127faee4c7179d8
Joshua Coutinho <jrc12@imperial.ac.uk>
2015-10-28 22:58:44 +0000

    Added Main.hs to check that project compiles

---
+++ HSRTool/Main.hs
@@ -0,0 +1,10 @@
+module Main where
+
+import HSRTool.CodeGen.Types
+import HSRTool.CodeGen.IntermStmt
+import HSRTool.CodeGen.SMTForm
+import HSRTool.CodeGen.CGExpr
+import HSRTool.Parser.Parser
+import HSRTool.Parser.Types
+import HSRTool.Test.TestE2E
+import HSRTool.Test.TestIntermForm
