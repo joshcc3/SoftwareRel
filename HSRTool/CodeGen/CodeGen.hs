abf08e335e78d2ec79d4eb8f36a9b2c7e92452f0
Joshua Coutinho <jrc12@imperial.ac.uk>
2015-10-30 15:27:03 +0000

    Added implementation of code-generation to accumulate assumptions

--- HSRTool/CodeGen/CodeGen.hs
+++ HSRTool/CodeGen/CodeGen.hs
@@ -31,23 +31,84 @@ transProg = pPDecls.traverse %~ g
       g p = case partitionEithers (map genStat (_pPrepost p)) of
               (ls, rs) -> p & pStmts %~ (ls ++) . (++ rs)
 
-toSSA :: Stmt NewId a -> Pred Op NewId -> SSA Op NewId
-toSSA (SAssignStmt (AssignStmt _ v e)) p  = [SSAAssign v (NE e)]
-toSSA (SBlockStmt _ l) p = l >>= \x -> toSSA x p
-toSSA (SIfStmt (IfStmt _ e tn (Just el))) p =
+stmtToSSA :: Stmt NewId (Pred Op NewId, Assumption Op NewId) -> SSA Op NewId
+stmtToSSA (SAssignStmt (AssignStmt _ v e)) = [SSAAssign v (NE e)]
+stmtToSSA (SBlockStmt _ l) = l >>= stmtToSSA
+stmtToSSA (SIfStmt (IfStmt _ e tn el)) =
   let
-      tnInstr = tn >>= \x -> toSSA x (EBinOp LAnd (Pair p e))
-      elInstr = el >>= \x -> toSSA x (EBinOp LAnd  (Pair p (EUnOp LNot (UnOp e))))
+      tnInstr = tn >>= stmtToSSA
+      elInstr = maybe [] id el >>= stmtToSSA
   in tnInstr ++ elInstr
-  -- tell [SSAAssign newId (NE $ EShortIf newPred (EID $ lkup m' v) (EID $ lkup m'' v))]
-toSSA (SIfStmt (IfStmt _ e tn Nothing)) p =
-  tn >>= \x -> toSSA x (EBinOp LAnd (Pair p e))
-toSSA (SAssumeStmt (AssumeStmt _ e)) p = []
---  ass %= \x -> NEBinOp LAnd x (NE p) :=> NE (apply e mp)
-toSSA (SAssertStmt (AssertStmt _ e)) p 
-    = [SSAAssert ((NE p) :=> NE e)]
-{-    =  [SSAAssert ((NEBinOp LAnd (NE p) assmpts) :=> NE e)]
-       where 
-         assmpts = undefined-}
-toSSA _ _ = []
+stmtToSSA s@(SAssertStmt (AssertStmt _ e))
+    = let (p, a) = extract s
+      in [SSAAssert ((NEBinOp LAnd (NE p) a) :=> NE e)]
+stmtToSSA _ = []
+
+-- (extract <$> a^?_Outer._2)^?_Just._Left._Left
+{-
+accumPred (SIfStmt' a)
+    = either' (either' enterIf atThen) (either' atElse exitIf) (extract a)
+      where 
+        enterIf _ = let v = extract a
+                    in openScope >> id %= updatePred v >> listToMaybe <$> get
+        atThen _ = get
+        atElse _ = let v = extract a
+                   in do
+                     closeScope
+                     openScope 
+                     id %= updatePred (EUnOp LNot (UnOp v))
+                     listToMaybe <$> get
+        exitIf = (\_ -> closeScope)
+        updatePred mv p' = maybe p' (:p') $ do
+                             p <- listToMaybe p'
+                             newPred <- mv
+                             return (EBinOp LAnd (Pair p newPred))              
+accumPred (SAssumeStmt (AssumeStmt _ e))
+    = listToMaybe <$> get
+--  id %= undefined -- \x -> NEBinOp LAnd x undefined {-(NE p)-} :=> NE undefined -- p
+accumPred _ = listToMaybe <$> get
+-}
+
+accumAssump :: Stmt String a -> 
+              State [(Pred Op String, Assumption Op String)] 
+                    (Maybe (Pred Op String, Assumption Op String))
+accumAssump (SIfStmt' a)
+  = either' (either' enterIf atThen) (either' atElse exitIf) (extract a)
+      where 
+        enterIf _ = listToMaybe <$> get
+        atThen _ = do
+          let bExpr = (extract <$> a^?_Outer._2)^?_Just._Left._Right._1
+              ifBExpr = maybe (error "Couldn't find bool expr in if") id bExpr
+          openScope
+          ix 0._1 %= \p ->  EBinOp LAnd (Pair ifBExpr p)
+          listToMaybe <$> get
+        atElse _ = do
+          closeScope
+          let bExpr' = (extract <$> a^?_Outer._2)^?_Just._Right._Just._1
+              bExpr = maybe (error "Couldn't find bool expr in else") id bExpr'
+              elseBExpr = EUnOp LNot (UnOp bExpr)
+          openScope
+          ix 0._1 %= \p -> EBinOp LAnd (Pair p elseBExpr)
+          listToMaybe <$> get
+        exitIf _ = closeScope
+accumAssump (SAssumeStmt (AssumeStmt _ e))
+    = do
+  ix 0 %= \(p, a) -> (p, NEBinOp LAnd a (NE p) :=> NE e)
+  listToMaybe <$> get
+accumAssump _ = listToMaybe <$> get
+
+openScope :: State [a] (Maybe a)
+openScope = do
+  id %= g
+  listToMaybe <$> get
+    where 
+      g [] = []
+      g (x:xs) = x:x:xs
+closeScope :: State [a] (Maybe a)
+closeScope = do
+  id %= g
+  listToMaybe <$> get
+      where 
+        g [] = []
+        g (x:xs) = xs
 
